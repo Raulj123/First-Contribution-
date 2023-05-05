57224396388339e7d5691a3f8521087cc57e2d43
IamRaul <jarquinr121@gmail.com>
2023-05-04 23:35:25 -0700

    inital commit, table done

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import '@picocss/pico'
+</script>
+
+<h1 style ="text-align: center; margin-top:30px; margin-bottom:10px;">Contribute to my Project!</h1>
+<p style="text-align:center; margin-top:0px;">Want to start your journey in Open Source? <a href="">click here</a></p>
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+  let contributors: info[] = [
+    {
+        name: "Raul",
+        github: "https://github.com/Raulj123"
+    }
+  ]
+
+</script>
+
+
+
+<table role="grid" >
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">GitHub Username</th>
+    </tr>
+  </thead>
+  {#each contributors as info}
+    <tbody>
+    <td>{info.name}</td>
+    <a href={info.github}>
+    <td>{info.name}'s Github</td>
+      </a>
+    </tbody>
+  {/each}
+</table>
+
+
+
+
+
+
+  <style>
+  table{
+    width:50%;
+    margin-left:auto;
+    margin-right:auto;
+  }
+</style>
