9484f335c1f28591ae16b3a496496885027f789d
dgjungle_c <dgjungle_c@163.com>
2023-05-09 09:36:00 +0800

    Add  an animation for contributors increasev😁

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
-import {contributors, totalContributors} from './contributors.ts'
+	import { contributors, totalContributors } from './contributors.ts';
 </script>
 
+<h3 style="text-align:center; margin-top:5px; margin-bottom:10px;">
+	Total Contributors {totalContributors}
+	<div class="upper">↑</div>
+</h3>
 
-<h3 style="text-align:center; margin-top:5px; margin-bottom:10px;">Total Contributors {totalContributors}</h3>
-
-<table role="grid" >
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">GitHub</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each contributors as contributor}
-      <tr>
-        <td>{contributor.name}</td>
-        <td>
-          <a href={contributor.github}>
-            {contributor.name}'s Github</a>
-        </td>
-      </tr>
-    {/each}
-  </tbody>
+<table role="grid">
+	<thead>
+		<tr>
+			<th scope="col">Name</th>
+			<th scope="col">GitHub</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each contributors as contributor}
+			<tr>
+				<td>{contributor.name}</td>
+				<td>
+					<a href={contributor.github}> {contributor.name}'s Github</a>
+				</td>
+			</tr>
+		{/each}
+	</tbody>
 </table>
 
-
-
-  <style>
-  table{
-    width:50%;
-    margin-left:auto;
-    margin-right:auto;
-  }
+<style>
+	table {
+		width: 50%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.upper {
+		display: inline-block;
+		color: red;
+		animation: upper-to 1000ms ease-in-out infinite;
+	}
+	@keyframes upper-to {
+		0% {
+			transform: translateY(0px);
+		}
+		100% {
+			transform: translateY(-5px);
+		}
+	}
 </style>
