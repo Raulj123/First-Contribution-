3db94554648bbd139dc65c9fa85d57f603d875c4
NISHANT KUMAR <nishantkumar19041@gmail.com>
2023-05-15 09:15:01 +0530

    Update contributors.ts
    resolved conflict

--- src/routes/contributors.ts
+++ src/routes/contributors.ts
@@ -2,21 +2,24 @@ export const contributors = [
 	{
 		name: 'Raul',
 		github: 'https://github.com/Raulj123'
-	}
+	},
 	// contributors add your code below this line
-	, {
+	{
 		name: 'Animesh Kumar',
 		github: 'https://github.com/wttc-nitr'
-	}
-	, {
+	},
+	{
 		name: 'Jungle Deng',
 		github: 'https://github.com/dgjungleP'
-	}
-	, {
-		name:'NepThunder',
+	},
+	{
+		name: 'NepThunder',
 		github: 'https://github.com/NepThunder'
-	}
-	, {
+	},
+	{ name: 'Yash Shekhar',
+	  github: 'https://github.com/Yash8840'
+	}, 
+        {
 		name:'Nishant Kumar',
 		github: 'https://github.com/NkOffiCiAL07'
 	}
