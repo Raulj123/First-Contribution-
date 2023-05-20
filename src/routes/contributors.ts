a07744656f0298aefa08ba1a92d724de5502a9e8
Cioraz <123grover222@gmail.com>
2023-05-20 23:15:19 +0530

    Added Name

--- src/routes/contributors.ts
+++ src/routes/contributors.ts
@@ -1,37 +1,41 @@
 export const contributors = [
-	{
-		name: 'Raul',
-		github: 'https://github.com/Raulj123'
-	},
-	// contributors add your code below this line
-	{
-		name: 'Animesh Kumar',
-		github: 'https://github.com/wttc-nitr'
-	},
-	{
-		name: 'Jungle Deng',
-		github: 'https://github.com/dgjungleP'
-	},
-	{
-		name: 'NepThunder',
-		github: 'https://github.com/NepThunder'
-	},
-	{
-		name: 'Yash Shekhar',
-		github: 'https://github.com/Yash8840'
-	},
-	{
-		name: 'Nishant Kumar',
-		github: 'https://github.com/NkOffiCiAL07'
-	},
-	{
-		name: 'Batuhan',
-		github: 'https://github.com/Nabzoku'
-	},
   {
-    name: 'Jinson',
-    github: 'https://github.com/FourAndHalf'
-  }
+    name: "Raul",
+    github: "https://github.com/Raulj123",
+  },
+  // contributors add your code below this line
+  {
+    name: "Animesh Kumar",
+    github: "https://github.com/wttc-nitr",
+  },
+  {
+    name: "Jungle Deng",
+    github: "https://github.com/dgjungleP",
+  },
+  {
+    name: "NepThunder",
+    github: "https://github.com/NepThunder",
+  },
+  {
+    name: "Yash Shekhar",
+    github: "https://github.com/Yash8840",
+  },
+  {
+    name: "Nishant Kumar",
+    github: "https://github.com/NkOffiCiAL07",
+  },
+  {
+    name: "Batuhan",
+    github: "https://github.com/Nabzoku",
+  },
+  {
+    name: "Jinson",
+    github: "https://github.com/FourAndHalf",
+  },
+  {
+    name: "Sunil Thunga",
+    github: "https://github.com/Cioraz",
+  },
 ];
 
 export const totalContributors = contributors.length;
